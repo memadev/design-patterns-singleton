2cac1733edc7373eb39aa0746978c8bd99f88a77
Mehdi Eshaghi <mema.eshaghi@gmail.com>
2018-08-29 11:26:35 +0200

    Lazy initialization of ContactService.

--- src/com/eshaghi/poc/designpatterns/singleton/ContactService.java
+++ src/com/eshaghi/poc/designpatterns/singleton/ContactService.java
@@ -2,16 +2,16 @@ package com.eshaghi.poc.designpatterns.singleton;
 
 public class ContactService {
 
-    private static final ContactService INSTANCE;
-
-    static {
-        INSTANCE = new ContactService();
-    }
+    private static ContactService INSTANCE;
 
     private ContactService() {
     }
 
     public static ContactService getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new ContactService();
+        }
+
         return INSTANCE;
     }
 }
